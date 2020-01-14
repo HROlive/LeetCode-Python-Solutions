14ab8fbc1eb7597ff4b96662ba8810780ecc60bd
Hugo Oliveira <35900662+HROlive@users.noreply.github.com>
2020-01-14 00:51:11 +0000

    Update 118-Pascal's_Triangle.py

--- 118-Pascal's_Triangle.py
+++ 118-Pascal's_Triangle.py
@@ -34,3 +34,14 @@ class Solution:
                     pascal[line][ele] = pascal[line-1][ele-1] + pascal[line-1][ele]
                 
         return pascal
+
+
+class Solution:
+def generate(self, numRows):
+    lists = []
+    for i in range(numRows):
+        lists.append([1]*(i+1))
+        if i>1 :
+            for j in range(1,i):
+                lists[i][j]=lists[i-1][j-1]+lists[i-1][j]
+    return lists
